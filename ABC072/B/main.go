855201cf1a9640545626e277f27aed7e25114c52
heutronica <heutronica@gmail.com>
2022-07-11 01:05:09 +0900

    🚀: Memory use and speed of runtime cut in half.

--- ABC072/B/main.go
+++ ABC072/B/main.go
@@ -1,27 +1,18 @@
-// 提出日時 	2022-07-11 00:46:36
-
-// 問題 	B - OddString
-// ユーザ 	heutronica
-// 言語 	Go (1.14.1)
-
-// 得点 	200
-// コード長 	193 Byte
-
-// 結果
-// 実行時間 	212 ms
-// メモリ 	7920 KB
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var S string
 	fmt.Scanf("%s", &S)
 
-	var word string
+	word := make([]string, (len(S)+1)/2)
 	for i := 0; i < len(S); i += 2 {
-		word += S[i : i+1]
+		word[i/2] = S[i : i+1]
 	}
-	fmt.Printf("%s\n", word)
+	answer := strings.Join(word, "")
+	fmt.Printf("%s\n", answer)
 }
