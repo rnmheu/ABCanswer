fc2b5f78596766547ecc537166b6a4f7d8fe7fa0
heutronica <heutronica@gmail.com>
2022-07-14 11:04:33 +0900

    ✨add: ABC259/A

--- ABC259/A/main.go
+++ ABC259/A/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	var N, M, X, T, D int
+	fmt.Scan(&N, &M, &X, &T, &D)
+
+	base := T - D*X
+
+	var ans int
+	if M <= X {
+		ans = base + M*D
+	} else if X < M {
+		ans = base + D*X
+	}
+
+	fmt.Println(ans)
 
 }
