9d3eea329053acb46c8364cfe5c7bcb4203ed50e
heutronica <heutronica@gmail.com>
2022-07-30 23:24:34 +0900

    🐛fix: Judgment when N==2(explainAC)

--- ARC/144/A.go
+++ ARC/144/A.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+
+	const (
+		initialBufSize = 10000
+		maxBufSize     = 1000000
+	)
+
+	sc := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, initialBufSize)
+	sc.Buffer(buf, maxBufSize)
+
 	var N int
 	var S string
-	fmt.Scan(&N, &S)
+
+	if sc.Scan() {
+		N, _ = strconv.Atoi(sc.Text())
+	}
+
+	if sc.Scan() {
+		S = sc.Text()
+	}
 
 	start := string(S[0])
 	end := string(S[N-1])
 
-	if (start == "A" && end == "B") || N == 2 {
+	if (start == "A" && end == "B") || S == "BA" || S == "AB" {
 		fmt.Println("No")
 	} else {
 		fmt.Println("Yes")
